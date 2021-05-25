c87d0908261c642c74b25dc4cd83f84fe864698b
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2021-05-25 14:24:40 +0200

    Project init

---
+++ src/main/java/Application.java
@@ -0,0 +1,23 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.VCARD;
+
+public class Application {
+    public static void main(String[] args) {
+        // some definitions
+        final String personURI    = "http://somewhere/JohnSmith";
+        final String fullName     = "John Smith";
+
+// create an empty Model
+        Model model = ModelFactory.createDefaultModel();
+
+// create the resource
+        Resource johnSmith = model.createResource(personURI);
+
+// add the property
+        johnSmith.addProperty(VCARD.FN, fullName);
+
+        System.out.println(model);
+    }
+}
